44314349353917a7f7aa37bebd53e95c70a8eb11
Srini6855 <srinipmps@gmail.com>
2024-08-02 17:41:08 +0530

    this is first commit

---
+++ src/test/java/com/omrbranch/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package com.omrbranch.runner;
+
+import com.omrbranch.base.BaseClass;
+import com.omrbranch.reporting.Reporting;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+
+import java.io.IOException;
+
+
+
+
+//public class TestRunner extends BaseClass {
+//    @AfterClass
+//    public static void generateReport () throws IOException {
+//        Reporting.generateReports (getProjectPath () + getPropertyValue ("jsonPath"));
+//    }
